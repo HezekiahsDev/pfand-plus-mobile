ac504a0a63fd0a8b9d4860df1e5105b22b9c4d3b
heze <heze>
2025-06-17 17:27:05 +0100

    Debugging EAS

--- pfand/lib/firebase-config.ts
+++ pfand/lib/firebase-config.ts
@@ -19,12 +19,12 @@ import { getReactNativePersistence, initializeAuth } from "firebase/auth";
  * @type {Object}
  */
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAbHpuSrdsLiU-UxtIdEinyHhGLv8uVGR4",
+  authDomain: "pfand-plus-be.firebaseapp.com",
+  projectId: "pfand-plus-be",
+  storageBucket: "pfand-plus-be.firebasestorage.app",
+  messagingSenderId: "510878772169",
+  appId: "1:510878772169:web:3ffdecf380b59bb9dfd618",
 };
 
 // ============================================================================
